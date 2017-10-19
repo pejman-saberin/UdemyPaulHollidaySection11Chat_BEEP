f4134fc7304f7e9ed4839748454be50722b98d04
pejman <naderpejman@yahoo.com>
2017-10-19 08:12:12 -0600

    147. Creating the Message Page
    On search user, when you search for a user it will take you to the messages screen where you can start conversation with that User. Once you click on the user, it will take you to the newly created messagePage, showing the first and last name of the user on the header

--- src/components/profile-search/profile-search.component.ts
+++ src/components/profile-search/profile-search.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component,Output,EventEmitter} from '@angular/core';
 import {Profile} from '../../models/profile/profile.interface';
 import {DataService} from '../../providers/data.service';
 
@@ -12,18 +12,25 @@ export class ProfileSearchComponent{
     query:string;
     profileList: Profile[]//we know that search query will have a list of profile back
 
-    constructor (private data: DataService){
+    @Output() selectedProfile: EventEmitter <Profile>
 
+    constructor (private data: DataService){
+      this.selectedProfile=new EventEmitter<Profile>();
     }
 
     searchUser(query:string){
-      const trimmedQuery=query.trim();
+      const trimmedQuery=query.trim();  //Simply disregard the white spaces after the name. Does not check for lower and upper case. More need to be implemented for a better search-->Maybe turn all the alphabets to upper case or lower case before you query the database
       if (trimmedQuery===query){
         this.data.searchUser(query).subscribe(profiles=>{
             console.log(profiles);
             this.profileList=profiles;
         })
       }
+    }
+
+    selectProfile(profile:Profile){
+      this.selectedProfile.emit(profile);
+
     }
 
 
--- src/pages/message/message.ts
+++ src/pages/message/message.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import {Profile} from "../../models/profile/profile.interface";
+import {Profile} from '../../models/profile/profile.interface';
+
 
 /**
- * Generated class for the SearchUserPage page.
+ * Generated class for the MessagePage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -11,20 +12,23 @@ import {Profile} from "../../models/profile/profile.interface";
 
 @IonicPage()
 @Component({
-  selector: 'page-search-user',
-  templateUrl: 'search-user.html',
+  selector: 'page-message',
+  templateUrl: 'message.html',
 })
-export class SearchUserPage {
+export class MessagePage {
+
+  selectedProfile:Profile;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  openChat(profile:Profile){
-    this.navCtrl.push('MessagePage',{profile});
-  }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SearchUserPage');
+    console.log('ionViewDidLoad MessagePage');
+  }
+
+  ionViewWillLoad(){
+    this.selectedProfile=this.navParams.get('profile');
   }
 
 }
